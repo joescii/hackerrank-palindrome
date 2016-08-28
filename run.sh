c006e611a38660126100385a1ff8bf20778eaca8
Joe Barnes <barnesjd@gmail.com>
2016-08-28 18:06:05 -0500

    Bootstrapped runnable project

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat ./src/test/resources/input.txt | sbt run
--- src/main/scala/PalindromeBuilder.scala
+++ src/main/scala/PalindromeBuilder.scala
@@ -4,12 +4,21 @@ import java.io.{BufferedReader, InputStream, InputStreamReader}
 
 import scala.util.Try
 
-object PalindromeBuilder {
-  def parseInput(is: InputStream): Option[List[(String, String)]] = Try {
+object PalindromeBuilder extends App {
+  type Input = List[(String, String)]
+  type Output = List[String]
+  def parseInput(is: InputStream): Option[Input] = Try {
     val r = new BufferedReader(new InputStreamReader(is))
     val lines = Iterator.continually(r.readLine()).takeWhile(_ != null).drop(1).toList.zipWithIndex
     val odds = lines.filter(_._2 % 2 == 1).map(_._1)
     val evens = lines.filter(_._2 % 2 == 0).map(_._1)
     evens zip odds
   }.toOption
+
+  def palindromes(a:String, b:String):String = {
+    "test"
+  }
+
+  val input = parseInput(System.in)
+  input.map(_.map { case (a, b) => System.out.println(palindromes(a, b))})
 }
