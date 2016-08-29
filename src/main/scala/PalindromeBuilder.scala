0746549478a68ddbe5834fb03875d1242f54b169
Joe Barnes <barnesjd@gmail.com>
2016-08-29 11:08:01 -0500

    Using List.parition thanks to the keen eyes of Roberto. Thanks

--- src/main/scala/PalindromeBuilder.scala
+++ src/main/scala/PalindromeBuilder.scala
@@ -8,9 +8,8 @@ object PalindromeBuilder extends App {
   def parseInput(is: InputStream): Option[List[(String, String)]] = Try {
     val r = new BufferedReader(new InputStreamReader(is))
     val lines = Iterator.continually(r.readLine()).takeWhile(_ != null).drop(1).toList.zipWithIndex
-    val odds = lines.filter(_._2 % 2 == 1).map(_._1)
-    val evens = lines.filter(_._2 % 2 == 0).map(_._1)
-    evens zip odds
+    val (evens, odds) = lines.partition(_._2 % 2 == 0)
+    evens.map(_._1) zip odds.map(_._1)
   }.toOption
 
   type Substring = (String, Option[Char])
